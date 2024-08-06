c643c360bf005a88c447abd1eea8cc51b4cae079
Renan Cavichi <renancavichi@gmail.com>
2024-08-06 17:45:26 -0300

    add routes: auth and account

--- src/server.js
+++ src/server.js
@@ -2,13 +2,30 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.json(
-        {
-            message: "Olá Mundo Express!",
-            success: "Sucesso ao executar API"
-        }
-    )
+app.post('/auth/signup', (req, res) => {
+    res.json({message: "Rota de POST Auth/Signup"})
+})
+app.post('/auth/login', (req, res) => {
+    res.json({message: "Rota de POST Auth/Login"})
+})
+app.post('/auth/logout', (req, res) => {
+    res.json({message: "Rota de POST Auth/Logout"})
+})
+
+app.post('/account', (req, res) => {
+    res.json({message: "Rota de POST Account"})
+})
+app.get('/account/list', (req, res) => {
+    res.json({message: "Rota de GET Account/List"})
+})
+app.get('/account/:id', (req, res) => {
+    res.json({message: "Rota de GET Account ID"})
+})
+app.put('/account/:id', (req, res) => {
+    res.json({message: "Rota de PUT Account"})
+})
+app.delete('/account/:id', (req, res) => {
+    res.json({message: "Rota de DELETE Account"})
 })
 
 app.listen(3000, () => {
