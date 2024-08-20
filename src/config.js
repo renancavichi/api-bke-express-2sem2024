02ad3f02f0e274a56b4bc8cb328703bbd341ddcd
Renan Cavichi <renancavichi@gmail.com>
2024-08-20 17:17:15 -0300

    add HOST and PORT env

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
 export const ENVIRONMENT = process.env.ENVIRONMENT || 'não definido'
+export const PORT = process.env.PORT || 3000
+export const HOST = process.env.HOST || 'http://localhost'
--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import authRouter from './routers/authRouter.js'
 import accountRouter from './routers/accountRouter.js'
-import { ENVIRONMENT } from './config.js'
+import { ENVIRONMENT, PORT, HOST } from './config.js'
 
 const app = express()
 
 app.use('/auth', authRouter)
 app.use('/account', accountRouter)
 
-app.listen(3000, () => {
-    console.log(`Servidor Rodando no ambiente ${ ENVIRONMENT }`)
+app.listen(PORT, () => {
+    console.log(`Servidor Rodando no ambiente ${ENVIRONMENT} em ${HOST}:${PORT}`)
 })
