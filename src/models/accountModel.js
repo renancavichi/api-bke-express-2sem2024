c1747f8fa3afe39f9a0035d47f5dba97b2f9891d
Renan Cavichi <renancavichi@gmail.com>
2024-09-17 17:02:54 -0300

    add order desc

--- src/models/accountModel.js
+++ src/models/accountModel.js
@@ -2,7 +2,11 @@ import {PrismaClient} from '@prisma/client'
 const prisma = new PrismaClient()
 
 export const listAccounts = async () => {
-    const accounts = await prisma.account.findMany()
+    const accounts = await prisma.account.findMany({
+        orderBy: {
+            id: 'desc'
+        }
+    })
     return accounts
 }
 
