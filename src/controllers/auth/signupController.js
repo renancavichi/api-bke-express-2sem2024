a380a1cde0f358519f8bc66173ba5ce10b3ae21d
Renan Cavichi <renancavichi@gmail.com>
2024-10-22 15:54:55 -0300

    add signup route

--- src/controllers/auth/signupController.js
+++ src/controllers/auth/signupController.js
@@ -1,5 +1,36 @@
-const signup = (req, res) => {
-    res.json({message: "Rota de POST Auth/Signup"})
+import { userValidateToCreate, signUp } from "../../models/userModel.js"
+import { v4 as uuid } from 'uuid'
+import bcrypt from "bcrypt"
+
+const signup = async (req, res, next) => {
+    try{
+        const newUser = req.body
+
+        const userValidated = userValidateToCreate(newUser)
+
+        if(userValidated?.error)
+            return res.status(401).json({
+                error: "Erro ao criar usuário!",
+                fieldErrors: userValidated.error.flatten().fieldErrors
+            })
+
+            userValidated.data.public_id = uuid()
+            userValidated.data.pass = bcrypt.hashSync(userValidated.data.pass, 10)
+
+        const result = await signUp(userValidated.data)
+
+        if(!result)
+            return res.status(401).json({
+                error: "Erro ao criar conta!"
+            })
+
+        return res.json({
+            success: "Conta criada com sucesso!",
+            user: result
+        })
+    } catch(error) {
+        next(error)
+    }
 }
 
 export default signup
---
+++ src/models/userModel.js
@@ -0,0 +1,62 @@
+import {PrismaClient} from '@prisma/client'
+import {z} from 'zod'
+
+const prisma = new PrismaClient()
+
+const accountSchema = z.object({
+    id: z.number({
+            invalid_type_error: "O id deve ser um valor numérico",
+            required_error: "O id é obrigatório"
+        })
+        .positive({message: "O id deve ser um número positivo maior que 0"}),
+    public_id: z.string({
+            invalid_type_error: "O public_id deve ser uma string",
+            required_error: "O public_id é obrigatório"
+        }),  
+    name: z.string({
+            invalid_type_error: "O nome deve ser uma string",
+            required_error: "O nome é obrigatório"
+        })
+        .min(3, {message: "O nome deve ter ao menos 3 caracter"})
+        .max(100, {message: "O nome deve ter no máximo 100 caracteres"}),
+    email: z.string({
+            invalid_type_error: "O email deve ser uma string",
+            required_error: "O email é obrigatório"
+        })
+        .email({message: "Email inválido"})
+        .max(200, {message: "O email deve ter no máximo 200 caracteres"}),    
+    avatar: z.string({
+            invalid_type_error: "O avatar deve ser uma string"
+        })
+        .url({message: "Url Inválida"})
+        .min(11, {message: "O avatar deve ter no mínimo 11 caracteres"})
+        .max(1000, {message: "O avatar deve ter no máximo 1000 caracteres"})
+        .optional(),
+    pass: z.string({
+            invalid_type_error: "A senha deve ser uma string",
+            required_error: "A senha é obrigatória"
+        })
+        .min(6, {message: "A senha deve ter ao menos 6 caracter"})
+        .max(500, {message: "A senha deve ter no máximo 500 caracteres"})
+})
+
+export const userValidateToCreate = (account) => {
+    const partialAccountSchema = accountSchema.partial({id: true, public_id: true})
+    return partialAccountSchema.safeParse(account)
+}
+
+export const getByPublicId = async (public_id) => {
+    const user = await prisma.user.findUnique({
+        where: {
+            public_id
+        }
+    })
+    return user
+}
+
+export const signUp = async (user) => {
+    const result = await prisma.user.create({
+        data: user
+    })
+    return result
+}
